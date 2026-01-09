191b1d196fd54506e990518d312dd3f3364fac90
VisionR1 <skellet64@gmail.com>
2026-01-09 21:30:09 +0200

    General Update
    - Update GitHub Actions
    
    - Use latest Apple Silicon for CI
    
    - Update SDK
    
    - Update NDK
    
    - Update SDK Build Tools
    
    - Update Gradle
    
    - Update AGP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,9 +4,13 @@ plugins {
 
 android {
     namespace = "es.chiteroman.playintegrityfix"
-    compileSdk = 36
-    ndkVersion = "28.2.13676358"
-    buildToolsVersion = "36.0.0"
+    compileSdk {
+        version = release(36) {
+            minorApiLevel = 1
+        }
+    }
+    ndkVersion = "29.0.14206865"
+    buildToolsVersion = "36.1.0"
 
     buildFeatures {
         prefab = true
